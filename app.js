aca6bfcbf178767ed0a8013abe1c6ea70a7b80d9
dip.himu@gmail.com <dip.himu@gmail.com>
2019-04-24 17:17:13 +0600

    chp-2 files only push

---
+++ app.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+	el : '#app',
+	data: {
+		title: sample.title,
+		address: sample.address,
+		about: sample.about,
+		prices: sample.prices,
+		amenities: sample.amenities,
+		headerImageStyle: {
+			'background-image':'url(sample/header.jpg)'
+		},
+		contracted: true,
+		modalOpen: false
+	},
+	methods:{
+						escapeKeyListener:function(evt) {
+							if(evt.keycode === 27 && app.modalOpen) {
+								app.modalOpen = false;
+							}
+						}
+	},
+	watch: {
+		modalOpen:function(){
+			var className = 'modal-open';
+			if(this.modalOpen){
+				document.body.classList.add(className);
+			}
+			else{
+				document.body.classList.remove(className);
+			}
+		}
+	},
+	created: function() {
+		document.addEventListener('keyup',this.escapeKeyListener);
+	},
+	destroyed: function () {
+document.removeEventListener('keyup', this.escapeKeyListener);
+}
+});
