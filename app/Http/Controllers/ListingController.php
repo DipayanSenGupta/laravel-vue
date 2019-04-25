4d747bbb51f6fdb7e0a5925fe97befb581e75703
dip.himu@gmail.com <dip.himu@gmail.com>
2019-04-25 15:13:32 +0600

    webpack added, bundled js

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Listing;
-class ListingCOntroller extends Controller
+class ListingController extends Controller
 {
 		public function get_listing_api(Listing $listing)
 		{
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,41 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
+import sample from './data';
+import Vue from 'vue';
+var app = new Vue({
+	el : '#app',
+	data: {
+		title: sample.title,
+		address: sample.address,
+		about: sample.about,
+		prices: sample.prices,
+		amenities: sample.amenities,
+		headerImageStyle: {
+			'background-image':'url(sample/header.jpg)'
+		},
+		contracted: true,
+		modalOpen: false
+	},
+	methods:{
+						escapeKeyListener:function(evt) {
+							if(evt.keycode === 27 && app.modalOpen) {
+								app.modalOpen = false;
+							}
+						}
+	},
+	watch: {
+		modalOpen:function(){
+			var className = 'modal-open';
+			if(this.modalOpen){
+				document.body.classList.add(className);
+			}
+			else{
+				document.body.classList.remove(className);
+			}
+		}
+	},
+	created: function() {
+		document.addEventListener('keyup',this.escapeKeyListener);
+	},
+	destroyed: function () {
+document.removeEventListener('keyup', this.escapeKeyListener);
+}
 });
